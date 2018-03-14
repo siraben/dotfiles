7fc2136ffd2c6b9e52f9488f7d7a81e722c103d9
Ben Phipathananunth <bensiraphob@hotmail.co.uk>
2018-03-14 18:23:04 +0700

    Add keybinding, make helm much smaller.

--- emacs/.emacs.d/modules/siraben-programming.el
+++ emacs/.emacs.d/modules/siraben-programming.el
@@ -3,6 +3,8 @@
 ;; This file makes managing the different programming modes that
 ;; need to be loaded easier.
 
+(global-set-key (kbd "M-C") 'comment-or-uncomment-region)
+
 (require 'siraben-js)
 (require 'siraben-c)
 (require 'siraben-lisp)
