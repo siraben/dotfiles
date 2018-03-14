b418b0413d62fcae461ae45bdef96f735328eede
Ben Phipathananunth <bensiraphob@hotmail.co.uk>
2018-03-14 21:38:04 +0700

    Change scheme progam in linux

--- emacs/.emacs.d/modules/siraben-linux.el
+++ emacs/.emacs.d/modules/siraben-linux.el
@@ -2,7 +2,7 @@
 
 ;; This file runs when the target OS is gnu/linux.
 
-(setq scheme-program-name "/usr/bin/mit-scheme")
+(setq scheme-program-name "/usr/bin/guile")
 (setq ispell-program-name "/usr/bin/aspell")
 
 (provide 'siraben-linux)
