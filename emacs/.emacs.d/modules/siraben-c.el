59d3297c1567913df4112e37e0ae6c3f1c812999
Ben Phipathananunth <bensiraphob@hotmail.co.uk>
2018-03-21 21:27:05 +0700

    Fix c-mode-map variable void + github-desktop not found

--- emacs/.emacs.d/modules/siraben-c.el
+++ emacs/.emacs.d/modules/siraben-c.el
@@ -1,7 +1,7 @@
 ;; siraben-c.el
 
 ;; Configures Emacs for C-code development
-
+(require 'cc-mode)
 (use-package irony)
 (use-package company-irony)
 (use-package flycheck-irony)
@@ -10,6 +10,6 @@
                            (irony-mode 1)
                            (flycheck-mode 1)))
 
-(define-key c-mode-map (kbd "s-b") #'recompile)
+(define-key c-mode-base-map (kbd "s-b") 'recompile)
 
 (provide 'siraben-c)
