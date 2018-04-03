04126e3d5ee3a1ba91c75b961153af531d386638
Ben Phipathananunth <bensiraphob@hotmail.co.uk>
2018-04-03 22:05:20 +0700

    siraben-preconf.el broke my Emacs!

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,37 @@
 
 ;; init.el
 
+;; Always prefer the newest version of a file.
+(setq load-prefer-newer t)
+
+;; Reducing garbage collection makes startup faster.
+(setq gc-cons-threshold 50000000)
+
+;; At least remove the eyesores while we wait.
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+(menu-bar-mode -1)
+
+(when window-system
+  (scroll-bar-mode -1))
+
+;; And ensure the cursor is a box, and remove the fringe.
+(setq-default cursor-type 'box)
+
+(fringe-mode 0)
+
+(defvar siraben-root-dir
+  "~/dotfiles/emacs/.emacs.d/"
+  "The root directory of the Emacs configuration.")
+
+(defvar siraben-modules-dir
+  (expand-file-name "modules" siraben-root-dir)
+  "The directory that contains all the modules for my
+configuration.")
+
+(add-to-list 'load-path siraben-modules-dir)
+
 ;; This is the first to be executed by Emacs.
 
 (custom-set-variables
@@ -21,7 +52,6 @@
 ;; longer be needed.
 ;; (package-initialize)
 
-(require 'siraben-preconf)
 (require 'siraben-core)
 (require 'siraben-packages)
 (require 'siraben-ui)
