ff8a5f64ca57359b11703e1f34b20c78660562d6
Ben Phipathananunth <bensiraphob@hotmail.co.uk>
2018-04-08 09:05:04 +0700

    Add Guix

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -41,6 +41,7 @@ configuration.")
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- setup-linux.sh
+++ setup-linux.sh
@@ -1,20 +1,46 @@
 #!/usr/bin/env bash
-echo "========================================================================"
+>echo "========================================================================"
 echo "|                       Ben's Linux Setup Script                       |"
 echo "========================================================================"
 
 echo "We'll need sudo permissions temporarily..."
 sudo echo "Successfully used sudo!" || exit
 
-sudo add-apt-repository -y ppa:dyatlov-igor/materia-theme
+# Verify Guix download
+wget ftp://alpha.gnu.org/gnu/guix/guix-binary-0.14.0.x86_64-linux.tar.xz
+wget ftp://alpha.gnu.org/gnu/guix/guix-binary-0.14.0.x86_64-linux.tar.xz.sig
+gpg --verify guix-binary-0.14.0.x86_64-linux.tar.xz.sig
 
-sudo apt update
+# Extract Guix
+sudo cd /tmp
+sudo tar --warning=no-timestamp -xf guix-binary-0.14.0.x86_64-linux.tar.xz
+sudo mv var/guix /var/ && mv gnu /
+sudo ln -sf /var/guix/profiles/per-user/root/guix-profile ~root/.guix-profile
 
-sudo apt install -y emacs git stow gcc zsh wget curl guile emacs25-common-non-dfsg
+
+sudo GUIX_PROFILE=$HOME/.guix-profile
+sudo source $GUIX_PROFILE/etc/profile
+
+# Make Guix avaliable for everyone.
+sudo mkdir -p /usr/local/bin
+sudo cd /usr/local/bin
+sudo ln -s /var/guix/profiles/per-user/root/guix-profile/bin/guix
+
+# Make info manual avaliable to everyone.
+sudo mkdir -p /usr/local/share/info
+sudo cd /usr/local/share/info
+sudo for i in /var/guix/profiles/per-user/root/guix-profile/share/info/* ; do ln -s $i ; done
+
+# Fix locale issue.
+guix package -i glibc-locales
+export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
+
+# Install Guix packages
+guix package -i guile emacs icecat stow git zsh make gcc font-hack
 
 # Setup Oh My Zsh!
-
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-
-sudo apt install materia-theme
+echo "========================================================================"
+echo "|                                 Done                                 |"
+echo "========================================================================"
