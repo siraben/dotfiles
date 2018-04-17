56c4b1d67d0a34596a941019bd4aaa052f004e00
Ben Phipathananunth <bensiraphob@hotmail.co.uk>
2018-04-17 15:06:40 +0700

    Fix no siraben-java.el, adding at a later commit.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -36,9 +36,17 @@ configuration.")
 ;; This is the first to be executed by Emacs.
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
 
 ;; Make package.el happy. Drop this line in Emacs 27 as it will no
