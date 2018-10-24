eb817d5dfed57ccdedb710f212a315ffbd52fe53
siraben <bensiraphob@gmail.com>
2018-10-24 14:31:43 +0700

    Fix Org HTML export

--- emacs/.emacs.d/modules/siraben-js.el
+++ emacs/.emacs.d/modules/siraben-js.el
@@ -20,18 +20,19 @@
 ;; This file handles working with JavaScript.
 
 ;;; Code:
-(use-package js2-mode
-  :config 
-  (add-to-list 'auto-mode-alist '("\\.js\\'"    . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.pac\\'"   . js2-mode))
-  (add-to-list 'interpreter-mode-alist '("node" . js2-mode)))
 
-(add-hook 'js-mode-hook
-          #'(lambda ()
-              (smartparens-strict-mode 1)
-              (js2-mode 1)
-              (setq-local electric-layout-rules '((?\; . after)))
-              (js2-imenu-extras-mode 1)))
+;; (use-package js2-mode
+;;   :config 
+;;   (add-to-list 'auto-mode-alist '("\\.js\\'"    . js2-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.pac\\'"   . js2-mode))
+;;   (add-to-list 'interpreter-mode-alist '("node" . js2-mode)))
+
+;; (add-hook 'js-mode-hook
+;;           #'(lambda ()
+;;               (smartparens-strict-mode 1)
+;;               (js2-mode 1)
+;;               (setq-local electric-layout-rules '((?\; . after)))
+;;               (js2-imenu-extras-mode 1)))
 
 (provide 'siraben-js)
 ;;; siraben-js.el ends here
