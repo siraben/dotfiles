b03c2834975b26cf9a23d7329143845f2549025c
siraben <bensiraphob@gmail.com>
2019-12-13 19:31:29 -0600

    Add Adga mode, hindent

---
+++ emacs/.emacs.d/modules/siraben-agda.el
@@ -0,0 +1,24 @@
+;;; siraben-agda.el --- configures Emacs for Agda development
+
+;;; License:
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(add-to-list 'auto-mode-alist
+             '(("\\.agda\\'" . agda2-mode)
+               ("\\.lagda.md\\'" . agda2-mode)))
+
+;;; siraben-agda.el ends here
