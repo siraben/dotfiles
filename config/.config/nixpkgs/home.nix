f588f09df58837c92f39f3d7c23c3bfb11de0f2c
Ben Siraphob <bensiraphob@gmail.com>
2020-08-19 20:40:16 +0700

    Add home-manager configuration

---
+++ config/.config/nixpkgs/home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "siraben";
+  home.homeDirectory = "/Users/siraben";
+  home.packages = with pkgs; [
+    ag
+    borgbackup
+    cabal-install
+    chez
+    ghc
+    guile
+    haskellPackages.haskell-language-server
+    htop
+    jq
+    mu
+    nodePackages.bash-language-server
+    nodePackages.node2nix
+    nodePackages.npm
+    nodejs
+    # python-language-server
+    python37Packages.pillow 
+    ranger
+    rustup
+    texlive.combined.scheme-small
+    the-powder-toy
+    tmux
+    tor
+    vim
+    watch
+    zile
+    zathura
+  ];
+  
+  programs = {
+    git.enable = true;
+    emacs.enable = true;
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.09";
+}
