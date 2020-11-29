33479c8cfd34ef838e614e79a35684c7c48acafb
Ben Siraphob <bensiraphob@gmail.com>
2020-11-29 22:03:02 +0700

    Add unfill-paragraph

--- emacs/.emacs.d/modules/siraben-editor.el
+++ emacs/.emacs.d/modules/siraben-editor.el
@@ -97,5 +97,14 @@ Enables auto-fill mode, spell checking and disables company mode."
   (setq dashboard-items '((recents . 10)))
   (dashboard-setup-startup-hook))
 
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
 (provide 'siraben-editor)
 ;;; siraben-editor.el ends here
