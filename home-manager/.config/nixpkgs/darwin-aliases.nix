af1bd879399f4916582f6261af9b3485948f4f4d
Ben Siraphob <bensiraphob@gmail.com>
2021-05-28 19:04:20 +0700

    home-manager: modularize

---
+++ home-manager/.config/nixpkgs/darwin-aliases.nix
@@ -0,0 +1,11 @@
+{}:
+
+{
+  # useful command to run sequenced after a long command, `nix build; sd`
+  sd = "say done";
+  # brew bundle, but make it like home manager
+  bb-check = "brew bundle check --global --verbose";
+  bb-gc = "brew bundle cleanup --global --force";
+  bb-switch = "brew bundle install --global --verbose";
+  bb-upgrade = "brew bundle install --global --verbose --upgrade";
+}
