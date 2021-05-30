db944cce017b111985d9e5b40b0fc54222e4adbd
Ben Siraphob <bensiraphob@gmail.com>
2021-05-30 16:33:31 +0700

    darwin-alises: add linuxShell

--- home-manager/.config/nixpkgs/darwin-aliases.nix
+++ home-manager/.config/nixpkgs/darwin-aliases.nix
@@ -8,4 +8,5 @@
   bb-gc = "brew bundle cleanup --global --force";
   bb-switch = "brew bundle install --global --verbose";
   bb-upgrade = "brew bundle install --global --verbose --upgrade";
+  linuxShell = ''docker run --rm -it lnl7/nix nix-shell -p nixFlakes --run "nix --experimental-features 'nix-command flakes' shell nixpkgs#nixUnstable"'';
 }
