e4a0d6873eebec8897f0bbc509e47604a97227a4
Ben Siraphob <bensiraphob@gmail.com>
2021-06-08 09:16:11 +0700

    Add minimal server config

---
+++ min-server/configuration.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }: {
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.cleanTmpDir = true;
+  networking.hostName = "ecs-1fc0";
+  networking.firewall.allowPing = true;
+  services.openssh.enable = true;
+
+  environment.systemPackages = with pkgs; [ vim git tmux htop direnv nix-direnv nixpkgs-review ];
+
+  nix = {
+    trustedUsers = [ "root" "siraben" ];
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  environment.pathsToLink = [
+    "/share/nix-direnv"
+  ];
+
+  users = {
+    users.siraben = {
+      shell = pkgs.zsh;
+      isNormalUser = true;
+      home = "/home/siraben";
+      description = "Ben Siraphob";
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDGSUM4kaMhd0SaR7qXXbdUtTRy4uC9cPqbpfJ3QP+zZZfeU/rMg4Gv8w10JmFfvrPWFCRgZ3su7ewN+We3rbmN2qDxArOPdzjBfQ/N33epz1Th3fpswdoLmyYtUxeugqGo9TM2e4K4OwJwJnrOvfbfqqhkwvCYgcHzjsA2I1tEThI6eKcYInhq8IOSmwtnGNvl77HrH6cnXcrX3OK9XVSeHVzJKVwzb0IDsdr2fUdwCQZnlfeVj/LuXlDn5hueLQbi5qzyMdI+KeQA3i2iu+aq35Yn7ubOZjQ0kM0uCcm6nhWp4bXtSFGA4Kj4GwOvpTVULSdIW7mu6f37/OTW9MyuJnsFYxJgDUMB8giH4LHEOI9ZhYpkrvO01Lh2igCCVe8GGqDkpu9OQEzWRnFdE3oFH9QbPSWtniX2ZWH/zkoxP2iVGxJkcOiOZGAEsF19skyaCDyu0ZwC8xGzu8S6ZZic+BHeeXXstiquMuTemlU8dqxtmo+cw2xo7JqSZu20EPKjlXz/V6cVTfPQXeH+ANRz4bihdTfHEIEmAXH9PU4vni63loJvSdGqTITUtmQDpeSu+e5qF48IX+Hu+x+Hr/1HhGVn1o2G1DjutM+9BobHiMAq+rh/tPMc1zGlsdCyXf3121WBOrFG4fD/ZCdJoMAZzKaqmaIrcLvQbEVCrRHXNw== bensiraphob@gmail.com"
+      ];
+    };
+  };
+}
