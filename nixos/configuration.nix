25c4e65e7aa50c80a9f5bb5d77054ab11197cd86
Ben Siraphob <bensiraphob@gmail.com>
2021-10-07 17:58:19 -0500

    ZFS + GNOME 40

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,35 +1,11 @@
 let
   sources = import ../nix/sources.nix;
-  pkgs = import sources.nixpkgs {
-    config.allowUnfreePredicate = pkg:
-      builtins.elem (lib.getName pkg) [
-        "broadcom-sta"
-        "facetimehd-firmware"
-      ];
-  };
+  pkgs = import sources.nixpkgs { };
   lib = pkgs.lib;
 in
-{
-  imports = [ /etc/nixos/hardware-configuration.nix ./rescue_boot.nix ];
 
-  boot = {
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
-    cleanTmpDir = true;
-    supportedFilesystems = [ "exfat" "btrfs" ];
-    kernelParams = [
-      "noibrs"
-      "noibpb"
-      "nopti"
-      "nospectre_v2"
-      "nospectre_v1"
-      "l1tf=off"
-      "nospec_store_bypass_disable"
-      "no_stf_barrier"
-      "mds=off"
-      "mitigations=off"
-    ];
-  };
+{
+  imports = [ ./hardware-configuration.nix ];
 
   # Make unfree software explicit
   nixpkgs.config.allowUnfreePredicate = pkg:
@@ -37,45 +13,20 @@ in
       "broadcom-sta"
       "facetimehd-firmware"
     ];
-
-  networking.networkmanager.enable = true;
-  networking.hostName = "siraben-nixos";
-  networking.nameservers = [ "1.0.0.1" "1.1.1.1" ];
-
-  sound.enable = true;
-  hardware = {
-    bluetooth = {
-      enable = true;
-      settings = {
-        General = {
-          ControllerMode = "dual";
-          Enable = lib.concatStringsSep "," [ "Source" "Sink" "Media" "Socket" ];
-        };
-      };
-    };
-    facetimehd.enable = true;
-    pulseaudio = {
-      enable = true;
-      package = pkgs.pulseaudioFull;
-      extraConfig = "load-module module-switch-on-connect";
-    };
-    enableRedistributableFirmware = true;
-    cpu.intel.updateMicrocode = true;
-  };
-  hardware.opengl.driSupport32Bit = true;
-  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
-  hardware.pulseaudio.support32Bit = true;
-  services.blueman.enable = true;
-  services.mbpfan = {
-    enable = true;
-    lowTemp = 58;
-    highTemp = 63;
-  };
-
-  powerManagement.enable = true;
-  powerManagement.powertop.enable = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.requestEncryptionCredentials = true;
 
   time.timeZone = "America/Chicago";
+
+  networking.hostId = "e6ff0de6";
+  networking.hostName = "siraben-nixos";
+  services.zfs.autoSnapshot.enable = true;
+  services.zfs.autoScrub.enable = true;
+
+  networking.networkmanager.enable = true;
+  networking.nameservers = [ "1.0.0.1" "1.1.1.1" ];
 
   fonts = {
     fontDir.enable = true;
@@ -99,82 +50,27 @@ in
       monospace = [
         "Hack"
       ];
-      # sansSerif = [
-      #   "DejaVu Sans"
-      #   "Noto Sans"
-      # ];
-      # serif = [
-      #   "DejaVu Serif"
-      #   "Noto Serif"
-      # ];
-    };
-
-  };
-
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.channel = "https://nixos.org/channels/nixos-unstable";
-
-  environment.systemPackages = with pkgs; [ i3status brightnessctl ];
-
-  virtualisation.docker.enable = true;
-  virtualisation.virtualbox.host.enable = true;
-
-  programs.zsh.enable = true;
-  programs.zsh.promptInit = "";
-  programs.ssh.startAgent = true;
-  location.provider = "geoclue2";
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-    extraPackages = with pkgs; [
-      swaylock
-      swayidle
-      wl-clipboard
-      mako
-      xwayland
-      wlsunset
-    ];
-  };
-
-  xdg = {
-    portal = {
-      enable = true;
-      # extraPortals = with pkgs; [
-      #   xdg-desktop-portal-gtk
-      # ];
-      # gtkUsePortal = true;
+      sansSerif = [
+        "DejaVu Sans"
+        "Noto Sans"
+      ];
+      serif = [
+        "DejaVu Serif"
+        "Noto Serif"
+      ];
     };
   };
-  services.pipewire.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-  services.logind.extraConfig = ''
-    HandlePowerKey=suspend
-  '';
+  # GNOME 40
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-  # Disable XHC1 wakeup signal to avoid resume getting triggered some time
-  # after suspend. Reboot required for this to take effect.
-  services = {
-    udev.extraRules = ''SUBSYSTEM=="pci", KERNEL=="0000:00:14.0", ATTR{power/wakeup}="disabled"'';
-    tlp.enable = true;
-  };
-
-  # Enable CUPS to print documents.
   services.printing.enable = true;
-  services.printing.drivers = with pkgs; [
-    brlaser
-    gutenprint
-    gutenprintBin
-  ];
 
-  services.gnome.gnome-keyring.enable = true;
-  services.xserver = {
-    enable = true;
-    displayManager.defaultSession = "sway";
-    xkbOptions = "ctrl:nocaps";
-    libinput.enable = true;
-    layout = "us";
-  };
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
   nix = {
     trustedUsers = [ "root" "siraben" ];
@@ -184,16 +80,23 @@ in
     '';
   };
 
+  services.xserver.libinput.enable = true;
+
+  environment.pathsToLink = [ "/share/zsh" ];
+  environment.shells = with pkgs; [ bashInteractive zsh ];
+
   users = {
     users.siraben = {
       shell = pkgs.zsh;
+      useDefaultShell = false;
       isNormalUser = true;
       home = "/home/siraben";
       description = "Ben Siraphob";
-      extraGroups = [ "audio" "wheel" "networkmanager" "vboxusers" "docker" ];
+      extraGroups = [ "wheel" "networkmanager" "dialout" ];
     };
   };
 
-  system.stateVersion = "21.05";
+  system.stateVersion = "21.11";
 
 }
+
