298d75a9d1c89e36f5a41bb369916cb5f96f3f2a
Ben Siraphob <bensiraphob@gmail.com>
2021-10-15 02:29:47 -0500

    programs.nix: disable broot and enable direnv caching

--- home-manager/.config/nixpkgs/programs.nix
+++ home-manager/.config/nixpkgs/programs.nix
@@ -41,11 +41,21 @@ in
   nix-index.enable = true;
   direnv = {
     enable = true;
+    stdlib = ''
+      : ''${XDG_CACHE_HOME:=$HOME/.cache}
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+              echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+              echo -n "$PWD" | shasum | cut -d ' ' -f 1
+          )}"
+      }
+    '';
     nix-direnv.enable = true;
     nix-direnv.enableFlakes = true;
     enableZshIntegration = true;
   };
-  broot.enable = true;
+  broot.enable = false;
   git = {
     enable = true;
     lfs.enable = true;
