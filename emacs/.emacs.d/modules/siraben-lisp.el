bb7391d50d6981b50f20908477a44dac09393644
Ben Siraphob <bensiraphob@gmail.com>
2021-10-29 14:32:20 -0500

    emacs: defer geiser loading with :defer

--- emacs/.emacs.d/modules/siraben-lisp.el
+++ emacs/.emacs.d/modules/siraben-lisp.el
@@ -43,18 +43,18 @@
   (add-hook hook #'(lambda ()
                      (siraben-enable-lisp-editing-modes))))
 
-(run-at-time 10 nil #'(lambda () (use-package geiser
-                                   :config
-                                   (setq geiserx-default-implementation 'guile)
-                                   ;; Enable some Lisp modes like paredit and rainbow delimiters, but no
-                                   ;; need to undo and auto complete.
-                                   (add-hook 'geiser-repl-mode-hook
-                                             (lambda ()
-                                               (siraben-enable-lisp-editing-modes)
-                                               (undo-tree-mode                 -1)
-                                               (paredit-mode                   +1)
-                                               (aggressive-indent-mode         -1)))))
-             )
+(use-package geiser
+  :defer 10
+  :config
+  (setq geiser-default-implementation 'guile)
+  ;; Enable some Lisp modes like paredit and rainbow delimiters, but no
+  ;; need to undo and auto complete.
+  :hook (geiser-repl-mode-hook .
+                               (lambda ()
+                                 (siraben-enable-lisp-editing-modes)
+                                 (undo-tree-mode                 -1)
+                                 (paredit-mode                   +1)
+                                 (aggressive-indent-mode         -1))))
 (add-hook 'ielm-mode-hook
           (lambda ()
             (siraben-enable-lisp-editing-modes)
