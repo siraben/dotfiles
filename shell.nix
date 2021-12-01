e074c2038dafbafb5730b82b5788c5560b93f6e1
Ben Siraphob <bensiraphob@gmail.com>
2021-12-01 13:57:54 -0600

    Pin home-manager and make switching reproducible
    - Remove home-manager channel from my systems

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  sources = import ./nix/sources.nix;
+  nixpkgs = sources.nixpkgs;
+  pkgs = import nixpkgs {};
+in
+
+pkgs.mkShell rec {
+  name = "home-manager-shell";
+
+  buildInputs = with pkgs; [
+    niv
+    (import sources.home-manager { inherit pkgs; }).home-manager
+  ];
+
+  shellHook = ''
+    export NIX_PATH="nixpkgs=${nixpkgs}:home-manager=${sources."home-manager"}"
+    export HOME_MANAGER_CONFIG="./home-manager/.config/nixpkgs/home.nix"
+  '';
+}
