6c1f446e565e1a934cef171ed8ad06bd113a313e
Ben Siraphob <bensiraphob@gmail.com>
2021-12-26 20:04:20 +0700

    siraben-haskell: update header comment and use lsp-deferred

--- emacs/.emacs.d/modules/siraben-haskell.el
+++ emacs/.emacs.d/modules/siraben-haskell.el
@@ -1,4 +1,4 @@
-;;; siraben-programming.el -- siraben's programming configuration
+;;; siraben-haskell.el -- Haskell programming customizations.
 
 ;;; License:
 
@@ -66,7 +66,7 @@
     (setq-local prettify-symbols-alist haskell-prettify-symbols-alist)
     (prettify-symbols-mode 1)
     (haskell-indentation-mode t)
-    (lsp))
+    (lsp-deferred)
 
   (add-hook 'haskell-mode-hook #'my-haskell-mode-hook)
   (add-hook 'inferior-haskell-mode-hook (lambda () (paredit-mode -1)))
