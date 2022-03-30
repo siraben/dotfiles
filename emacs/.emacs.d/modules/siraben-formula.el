4ba9fbf2bf378d5c6d3c86fcbdc04f705bb52128
Ben Siraphob <bensiraphob@gmail.com>
2022-03-29 20:30:31 -0500

    siraben-formula: cleanup

--- emacs/.emacs.d/modules/siraben-formula.el
+++ emacs/.emacs.d/modules/siraben-formula.el
@@ -3,10 +3,4 @@
   (kill-all-local-variables)
   (setq mode-name  		"Formula")
   (setq major-mode 		'formula-mode)
-  ;; Turn on font-lock mode
-  ;; (and formula-font-lock-mode (font-lock-mode))
-  ;; (font-lock-mode)
-
-  ;; Finally, run the hooks and be done.
-  (run-hooks 'formula-mode-hook)
-  )
+  (run-hooks 'formula-mode-hook))
