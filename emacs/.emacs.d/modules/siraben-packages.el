af10bb2410ecbb7b0bd7ed45699c8986bccbd2a4
Ben Siraphob <bensiraphob@gmail.com>
2022-05-28 09:41:56 -0700

    emacs: lsp adjustments

--- emacs/.emacs.d/modules/siraben-packages.el
+++ emacs/.emacs.d/modules/siraben-packages.el
@@ -196,7 +196,23 @@
 
 (use-package lsp-mode
   :config
-  (setq lsp-clients-clangd-args '("-j=4" "-log=error")))
+  (setq lsp-clients-clangd-args '("-j=4" "-log=error"))
+  (setq lsp-auto-guess-root t)
+  (setq lsp-log-io nil)
+  (setq lsp-restart 'auto-restart)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-enable-on-type-formatting nil)
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-signature-render-documentation nil)
+  (setq lsp-eldoc-hook nil)
+  (setq lsp-modeline-code-actions-enable nil)
+  (setq lsp-modeline-diagnostics-enable nil)
+  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq lsp-semantic-tokens-enable nil)
+  (setq lsp-enable-folding nil)
+  (setq lsp-enable-imenu nil)
+  (setq lsp-enable-snippet nil)
+  (setq lsp-idle-delay 0.5))
 
 (use-package lsp-ui)
 
