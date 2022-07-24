27e4e42f4d5cccdcfa12bc1bd6ac1bddce598b25
Ben Siraphob <bensiraphob@gmail.com>
2022-07-24 15:50:47 -0700

    Simplify prompt on dumb terminal
    Finally fixes slow TRAMP

--- home-manager/.config/nixpkgs/programs.nix
+++ home-manager/.config/nixpkgs/programs.nix
@@ -15,23 +15,26 @@ let
     fpath+=("${pkgs.pure-prompt}/share/zsh/site-functions")
     if [ "$TERM" != dumb ]; then
       autoload -U promptinit && promptinit && prompt pure
+      vterm_printf(){
+          if [ -n "$TMUX" ] && ([ "''${TERM%%-*}" = "tmux" ] || [ "''${TERM%%-*}" = "screen" ] ); then
+              # Tell tmux to pass the escape sequences through
+              printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+          elif [ "''${TERM%%-*}" = "screen" ]; then
+              # GNU screen (screen, screen-256color, screen-256color-bce)
+              printf "\eP\e]%s\007\e\\" "$1"
+          else
+              printf "\e]%s\e\\" "$1"
+          fi
+      }
+      vterm_prompt_end() {
+          vterm_printf "51;A$(whoami)@$(hostname):$(pwd)";
+      }
+      setopt PROMPT_SUBST
+      PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
+    else
+      unsetopt zle
+      PS1='$ '
     fi
-    vterm_printf(){
-        if [ -n "$TMUX" ] && ([ "''${TERM%%-*}" = "tmux" ] || [ "''${TERM%%-*}" = "screen" ] ); then
-            # Tell tmux to pass the escape sequences through
-            printf "\ePtmux;\e\e]%s\007\e\\" "$1"
-        elif [ "''${TERM%%-*}" = "screen" ]; then
-            # GNU screen (screen, screen-256color, screen-256color-bce)
-            printf "\eP\e]%s\007\e\\" "$1"
-        else
-            printf "\e]%s\e\\" "$1"
-        fi
-    }
-    vterm_prompt_end() {
-        vterm_printf "51;A$(whoami)@$(hostname):$(pwd)";
-    }
-    setopt PROMPT_SUBST
-    PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
   '';
 in
 
