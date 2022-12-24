6c440c6db0f2f8a5656876fab529a9376fadb706
Ben Siraphob <bensiraphob@gmail.com>
2022-12-24 20:19:08 +0700

    server: update config slightly

--- server/nextcloud.nix
+++ server/nextcloud.nix
@@ -5,14 +5,5 @@
   package = pkgs.nextcloud24;
   hostName = "cloud.siraben.dev";
   https = true;
-  config = {
-    overwriteProtocol = "https";
-    dbtype = "pgsql";
-    dbuser = "nextcloud";
-    dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
-    dbname = "nextcloud";
-    dbpassFile = "/var/nextcloud-db-pass";
-    adminpassFile = "/var/nextcloud-admin-pass";
-    adminuser = "root";
-  };
+  config.adminpassFile = "/var/nextcloud-admin-pass";
 }
