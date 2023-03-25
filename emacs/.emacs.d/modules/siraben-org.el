43b19b1a3d17b7b2bcd3c306dd005827843b81f2
Ben Siraphob <bensiraphob@gmail.com>
2023-03-24 20:43:54 -0500

    emacs: add scrartcl to org-latex-classes

--- emacs/.emacs.d/modules/siraben-org.el
+++ emacs/.emacs.d/modules/siraben-org.el
@@ -94,6 +94,12 @@
                                     ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                                     ("\\paragraph{%s}" . "\\paragraph*{%s}")
                                     ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (add-to-list 'org-latex-classes '("scrartcl" "\\documentclass[11pt]{scrartcl}"
+                                    ("\\section{%s}" . "\\section*{%s}")
+                                    ("\\subsection{%s}" . "\\subsection*{%s}")
+                                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                                    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                                    ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
   )
 
 (use-package gnuplot)
