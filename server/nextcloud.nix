01d6e55bbbe2dd76bbb6380062e728cee0fd8421
Ben Siraphob <bensiraphob@gmail.com>
2023-11-07 00:09:38 -0500

    server: update nextcloud to 26

--- server/nextcloud.nix
+++ server/nextcloud.nix
@@ -2,7 +2,7 @@
 
 {
   enable = true;
-  package = pkgs.nextcloud25;
+  package = pkgs.nextcloud26;
   hostName = "cloud.siraben.dev";
   https = true;
   config.adminpassFile = "/var/nextcloud-admin-pass";
