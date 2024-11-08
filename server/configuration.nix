f0f81ea856647f79eb56df86563692c74f21e9ab
Ben Siraphob <bensiraphob@gmail.com>
2024-11-07 23:35:29 -0600

    server: sync updates from server

--- server/configuration.nix
+++ server/configuration.nix
@@ -1,49 +1,45 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+      "${fetchTarball {
+    url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
+    sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";}}/nextcloud-extras.nix"
+  ];
 
-let
-  publicKey = pkgs.fetchurl {
-    url = "https://github.com/siraben.keys";
-    sha256 = "sha256-i8gigitlRxiNa8eMPkD6FTOlVEO0p9LxRrBN41d/sJM=";
-  };
-in
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+  networking.hostName = "nixos";
+  networking.domain = "subnet03111413.vcn03111413.oraclevcn.com";
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDGSUM4kaMhd0SaR7qXXbdUtTRy4uC9cPqbpfJ3QP+zZZfeU/rMg4Gv8w10JmFfvrPWFCRgZ3su7ewN+We3rbmN2qDxArOPdzjBfQ/N33epz1Th3fpswdoLmyYtUxeugqGo9TM2e4K4OwJwJnrOvfbfqqhkwvCYgcHzjsA2I1tEThI6eKcYInhq8IOSmwtnGNvl77HrH6cnXcrX3OK9XVSeHVzJKVwzb0IDsdr2fUdwCQZnlfeVj/LuXlDn5hueLQbi5qzyMdI+KeQA3i2iu+aq35Yn7ubOZjQ0kM0uCcm6nhWp4bXtSFGA4Kj4GwOvpTVULSdIW7mu6f37/OTW9MyuJnsFYxJgDUMB8giH4LHEOI9ZhYpkrvO01Lh2igCCVe8GGqDkpu9OQEzWRnFdE3oFH9QbPSWtniX2ZWH/zkoxP2iVGxJkcOiOZGAEsF19skyaCDyu0ZwC8xGzu8S6ZZic+BHeeXXstiquMuTemlU8dqxtmo+cw2xo7JqSZu20EPKjlXz/V6cVTfPQXeH+ANRz4bihdTfHEIEmAXH9PU4vni63loJvSdGqTITUtmQDpeSu+e5qF48IX+Hu+x+Hr/1HhGVn1o2G1DjutM+9BobHiMAq+rh/tPMc1zGlsdCyXf3121WBOrFG4fD/ZCdJoMAZzKaqmaIrcLvQbEVCrRHXNw== bensiraphob@gmail.com'' ];
 
-{
-  imports = [ ./hardware-configuration.nix ];
-
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-
-  networking.hostName = "siraben-land";
-
-  networking.useDHCP = true;
-
-  users.users.siraben = {
-    shell = pkgs.zsh;
-    useDefaultShell = false;
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keyFiles = [ publicKey ];
-  };
 
   nix = {
     settings.trusted-users = [ "root" "siraben" ];
-    package = pkgs.nixUnstable;
-    optimise.automatic = true;
-    gc = {
-      automatic = true;
-      dates = "weekly";
-    };
+    package = pkgs.nixFlakes;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
   };
 
-  environment.systemPackages = with pkgs; [
-    vim git tmux
-  ];
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;    
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
 
-  services.openssh.enable = true;
-  services.openssh.settings.PasswordAuthentication = false;
+  environment.systemPackages = with pkgs; [ vim git htop ];
+  programs.mosh.enable = true;
+  programs.zsh.enable = true;
+  services.tailscale.enable = true;
+
+  users.users.siraben = {
+      shell = pkgs.zsh;
+      isNormalUser = true;
+      home = "/home/siraben";
+      description = "Ben Siraphob";
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDGSUM4kaMhd0SaR7qXXbdUtTRy4uC9cPqbpfJ3QP+zZZfeU/rMg4Gv8w10JmFfvrPWFCRgZ3su7ewN+We3rbmN2qDxArOPdzjBfQ/N33epz1Th3fpswdoLmyYtUxeugqGo9TM2e4K4OwJwJnrOvfbfqqhkwvCYgcHzjsA2I1tEThI6eKcYInhq8IOSmwtnGNvl77HrH6cnXcrX3OK9XVSeHVzJKVwzb0IDsdr2fUdwCQZnlfeVj/LuXlDn5hueLQbi5qzyMdI+KeQA3i2iu+aq35Yn7ubOZjQ0kM0uCcm6nhWp4bXtSFGA4Kj4GwOvpTVULSdIW7mu6f37/OTW9MyuJnsFYxJgDUMB8giH4LHEOI9ZhYpkrvO01Lh2igCCVe8GGqDkpu9OQEzWRnFdE3oFH9QbPSWtniX2ZWH/zkoxP2iVGxJkcOiOZGAEsF19skyaCDyu0ZwC8xGzu8S6ZZic+BHeeXXstiquMuTemlU8dqxtmo+cw2xo7JqSZu20EPKjlXz/V6cVTfPQXeH+ANRz4bihdTfHEIEmAXH9PU4vni63loJvSdGqTITUtmQDpeSu+e5qF48IX+Hu+x+Hr/1HhGVn1o2G1DjutM+9BobHiMAq+rh/tPMc1zGlsdCyXf3121WBOrFG4fD/ZCdJoMAZzKaqmaIrcLvQbEVCrRHXNw== bensiraphob@gmail.com"
+      ];
+    };
 
   services.nginx = import ./nginx.nix { };
 
@@ -55,22 +51,15 @@ in
   security.sudo.wheelNeedsPassword = false;
 
   services.nextcloud = import ./nextcloud.nix { inherit pkgs; };
-
+  systemd.services."nextcloud-setup" = {                                                   
+    requires = ["postgresql.service"];                                                     
+    after = ["postgresql.service"];                                                        
+  };
   services.postgresql = import ./postgresql.nix { };
 
-  environment.pathsToLink = [ "/share/zsh" ];
-  environment.shells = with pkgs; [ bashInteractive zsh ];
-
-  programs.mosh.enable = true;
-  programs.zsh.enable = true;
-  services.tailscale.enable = true;
-  systemd.services."nextcloud-setup" = {
-    requires = ["postgresql.service"];
-    after = ["postgresql.service"];
-  };
+  services.anki-sync-server = import ./anki-sync-server.nix { };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
-  networking.firewall.allowedUDPPorts = [ 80 443 ];
-  system.stateVersion = "23.11";
-
+  networking.firewall.allowedUDPPorts = [ ];
+  system.stateVersion = "24.05";
 }
