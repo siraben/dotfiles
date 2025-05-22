a583109c31b76fd011a6d206d756493daaa02da6
Ben Siraphob <bensiraphob@gmail.com>
2025-05-21 22:44:56 -0400

    home-manager: remove coq, move some packages around

--- home-manager/.config/nixpkgs/wrapWeb.nix
+++
@@ -1,7 +0,0 @@
-{ lib, writeShellScriptBin, ungoogled-chromium }:
-
-name: url: writeShellScriptBin name ''
-  ${ungoogled-chromium}/bin/chromium \
-    --app=${lib.escapeShellArg url} \
-    --user-data-dir=$HOME/.local/web-wrapper/${name}
-''
