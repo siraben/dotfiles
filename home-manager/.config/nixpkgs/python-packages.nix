afe9a7aa6b0844464191d0b2e73e40403b273055
Ben Siraphob <bensiraphob@gmail.com>
2025-11-02 21:08:46 -0800

    home-manager: fix naming

--- home-manager/.config/nixpkgs/python-packages.nix
+++ home-manager/.config/nixpkgs/python-packages.nix
@@ -12,7 +12,7 @@ pkgs.python313.withPackages (p: with p; [
   setuptools # setup.py
   scipy
   scikit-learn
-  z3 # Z3 theorem prover
+  z3-solver # Z3 theorem prover
 
   # for lsp bridge
   epc orjson sexpdata six paramiko rapidfuzz watchdog packaging
