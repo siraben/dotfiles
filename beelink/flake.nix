b35ceaad2f727cfa8d733b753211a9f51f8f709f
Ben Siraphob <bensiraphob@gmail.com>
2025-11-18 21:14:35 -0800

    beelink: use minimal hm config

--- beelink/flake.nix
+++ beelink/flake.nix
@@ -10,22 +10,27 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-      beelink = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./hardware-configuration.nix
-          
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.siraben = import ./home.nix;
-          }
-        ];
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      username = "siraben";
+    in
+    {
+      nixosConfigurations = {
+        beelink = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            ./hardware-configuration.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.siraben = import ../home-manager/.config/nixpkgs/home.nix;
+              home-manager.extraSpecialArgs = { inherit username inputs; minimal = true; };
+            }
+          ];
+        };
       };
     };
-  };
 }
