f8911d58bc22baabce49a305f56967c4fe8df6c4
Ben Siraphob <bensiraphob@gmail.com>
2026-02-10 18:15:28 -0500

    home-manager: fix rapidfuzz on darwin

--- home-manager/.config/nixpkgs/overlay.nix
+++ home-manager/.config/nixpkgs/overlay.nix
@@ -1,4 +1,19 @@
 final: prev:
 
 {
+  python3 = prev.python3.override {
+    packageOverrides = pyFinal: pyPrev: {
+      rapidfuzz = pyPrev.rapidfuzz.overridePythonAttrs (old: {
+        postPatch = (old.postPatch or "") + prev.lib.optionalString prev.stdenv.isDarwin ''
+          substituteInPlace CMakeLists.txt \
+            --replace-fail "set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)" \
+                           "set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
+          set(CMAKE_CXX_SCAN_FOR_MODULES OFF)"
+          substituteInPlace src/rapidfuzz/CMakeLists.txt \
+            --replace-fail "if(NOT Windows)" \
+                           "if(FALSE)"
+        '';
+      });
+    };
+  };
 }
