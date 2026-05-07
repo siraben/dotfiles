9010baaf5811b62cdda43f2cc496a6b98e213ac1
Ben Siraphob <bensiraphob@gmail.com>
2026-05-07 16:40:30 -0700

    Revert "server-arm: drop irc.siraben.dev, soju serves wss-only via chat.siraben.dev"
    This reverts commit 31756c20ff7b284565f978d90b2f7d72ceacab93.

--- server-arm/soju.nix
+++ server-arm/soju.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 
 let
+  ircHost = "irc.siraben.dev";
   webHost = "chat.siraben.dev";
 
   gamjaWeb = pkgs.gamja.override {
@@ -16,10 +17,25 @@ in
 {
   services.soju = {
     enable = true;
-    hostName = webHost;
-    listen = [ "ws+insecure://127.0.0.1:8080" ];
+    hostName = ircHost;
+    listen = [
+      "ircs://:6697"
+      "ws+insecure://127.0.0.1:8080"
+    ];
+    tlsCertificate = "/var/lib/acme/${ircHost}/fullchain.pem";
+    tlsCertificateKey = "/var/lib/acme/${ircHost}/key.pem";
     httpOrigins = [ "https://${webHost}" ];
     acceptProxyIP = [ "localhost" ];
+  };
+
+  # ACME cert dir for an nginx-enableACME host is mode 750 acme:nginx, so
+  # soju (DynamicUser=true) joins the nginx group via SupplementaryGroups.
+  systemd.services.soju.serviceConfig.SupplementaryGroups = [ "nginx" ];
+
+  services.nginx.virtualHosts.${ircHost} = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/".return = "204";
   };
 
   services.nginx.virtualHosts.${webHost} = {
@@ -31,4 +47,6 @@ in
       proxyWebsockets = true;
     };
   };
+
+  networking.firewall.allowedTCPPorts = [ 6697 ];
 }
