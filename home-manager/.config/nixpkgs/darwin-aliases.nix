e7457f5e6e33f63d4b523dd27a5d9f61a56eccb4
Ben Siraphob <bensiraphob@gmail.com>
2026-09-18 08:15:08 -0700

    zsh: remove unused Homebrew bundle aliases

--- home-manager/.config/nixpkgs/darwin-aliases.nix
+++ home-manager/.config/nixpkgs/darwin-aliases.nix
@@ -3,11 +3,6 @@
 {
   # useful command to run sequenced after a long command, `nix build; sd`
   sd = "say done";
-  # brew bundle, but make it like home manager
-  bb-check = "brew bundle check --global --verbose";
-  bb-gc = "brew bundle cleanup --global --force";
-  bb-switch = "brew bundle install --global --verbose";
-  bb-upgrade = "brew bundle install --global --verbose --upgrade";
   tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
   linuxShell = ''docker run --rm -it lnl7/nix nix-shell -p nixFlakes --run "nix --experimental-features 'nix-command flakes' shell nixpkgs#nixUnstable"'';
 }
